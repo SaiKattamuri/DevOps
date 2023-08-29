aed4af807c52b52f3b38d9ef967138e4a8d065b5
SaiKattamuri <50263861+SaiKattamuri@users.noreply.github.com>
2023-08-29 16:02:05 +0530

    Update Shell.sh

--- Linux/Shell.sh
+++ Linux/Shell.sh
@@ -39,4 +39,25 @@ echo "We use the CLI to interact with the OS, so how do we interact? we use comm
 2. External Commands - packages add-ons from OS flavor repos mv, cp, uptime, date"
 
 While going further we need to understand the file system of OS, in the Linux filesystem, every directory has its own purpose.
- 
+
+ / (root filesystem): It is the top-level filesystem directory. It must include every file needed to boot the Linux system before another filesystem is mounted. Every other filesystem is mounted on a well-defined and standard mount point because of the root filesystem directories after the system is started.
+/boot: It includes the static kernel and bootloader configuration and executable files needed to start a Linux computer.
+/bin: This directory includes user executable files.
+/dev: It includes the device file for all hardware devices connected to the system. These aren't device drivers; instead, they are files that indicate all devices on the system and provide access to these devices.
+/etc: It includes the local system configuration files for the host system.
+/lib: It includes shared library files that are needed to start the system.
+/home: The home directory storage is available for user files. All users have a subdirectory inside /home.
+/mnt: It is a temporary mount point for basic filesystems that can be used at the time when the administrator is working or repairing a filesystem.
+/media: A place for mounting external removable media devices like USB thumb drives that might be linked to the host.
+/opt: It contains optional files like vendor-supplied application programs that must be placed here.
+/root: It's the home directory for a root user. Keep in mind that it's not the '/' (root) file system.
+/tmp: It is a temporary directory used by the OS and several programs for storing temporary files. Also, users may temporarily store files here. Remember that files may be removed without prior notice at any time in this directory.
+/sbin: These are system binary files. They are executables utilized for system administration.
+/usr: They are read-only and shareable files, including executable libraries and binaries, man files, and several documentation types.
+/var: Here, variable data files are saved. It can contain things such as MySQL, log files, other database files, email inboxes, web server data files, and much more.
+
+Some Important symbols and their meanings:
+
+~   home
+$   normal user login
+#    root user login
