84abb57f06450064c44ddb9f36c03606bc2f3047
SaiKattamuri <50263861+SaiKattamuri@users.noreply.github.com>
2023-08-29 17:03:50 +0530

    Create Command-line_help.sh

---
+++ Linux/Command-line_help.sh
@@ -0,0 +1,12 @@
+# While working with commands, we may lost how to use them and what it does
+# Linux has a good documentation to understand the commands
+
+whatis date 
+# Here whatis is a command which helps to print the overview of the another command like date
+
+Man date
+# To get the full details of command we can use 'Man', which gives the manual for the command specified with overview, different options and arguments required for the options with set of examples
+
+date -h
+date --help
+# Several commands will provide -h or --help to provide users with the options and usecases available in a command
